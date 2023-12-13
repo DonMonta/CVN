c95f24c7757287a519ff593933ece73cea0645de
AndreinaCrespo-Mapache18 <karla.crespo.casierra@utelvt.edu.ec>
2023-12-12 22:19:06 -0500

    Actualización de Carrusel de Carreras

--- CVN PAGINA PRINCIPAL/assets/js/custom.js
+++ CVN PAGINA PRINCIPAL/assets/js/custom.js
@@ -1,108 +1,108 @@
-(function($) {
+(function ($) {
 
-	$(document).ready(function() {
-	  $('body').addClass('js');
-	  var $menu = $('#menu'),
-	    $menulink = $('.menu-link');
-	  
-	$menulink.click(function() {
-	  $menulink.toggleClass('active');
-	  $menu.toggleClass('active');
-	  return false;
-	});});
+	$(document).ready(function () {
+		$('body').addClass('js');
+		var $menu = $('#menu'), $menulink = $('.menu-link');
+
+		$menulink.click(function () {
+			$menulink.toggleClass('active');
+			$menu.toggleClass('active');
+			return false;
+		});
+	});
 
 
 	videoPopup();
 
 
 	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    margin:30,
-	    nav:true,
-	    autoplay:true,
-		autoplayTimeout:5000,
-		autoplayHoverPause:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        550:{
-	            items:2
-	        },
-	        750:{
-	            items:3
-	        },
-	        1000:{
-	            items:4
-	        },
-	        1200:{
-	            items:5
-	        }
-	    }
-	})
+		loop: true,
+		margin: 30,
+		nav: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			550: {
+				items: 2
+			},
+			750: {
+				items: 3
+			},
+			1000: {
+				items: 4
+			},
+			1200: {
+				items: 5
+			}
+		}
+	});
 
 
 	$(".Modern-Slider").slick({
-	    autoplay:true,
-	    autoplaySpeed:10000,
-	    speed:600,
-	    slidesToShow:1,
-	    slidesToScroll:1,
-	    pauseOnHover:false,
-	    dots:true,
-	    pauseOnDotsHover:true,
-	    cssEase:'fade',
-	   // fade:true,
-	    draggable:false,
-	    prevArrow:'<button class="PrevArrow"></button>',
-	    nextArrow:'<button class="NextArrow"></button>', 
+		autoplay: true,
+		autoplaySpeed: 10000,
+		speed: 600,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		pauseOnHover: false,
+		dots: true,
+		pauseOnDotsHover: true,
+		cssEase: 'fade',
+		// fade:true,
+		draggable: false,
+		prevArrow: '<button class="PrevArrow"></button>',
+		nextArrow: '<button class="NextArrow"></button>',
 	});
 
 
 	$("div.features-post").hover(
-	    function() {
-	        $(this).find("div.content-hide").slideToggle("medium");
-	    },
-	    function() {
-	        $(this).find("div.content-hide").slideToggle("medium");
-	    }
-	 );
+		function () {
+			$(this).find("div.content-hide").slideToggle("medium");
+		},
+		function () {
+			$(this).find("div.content-hide").slideToggle("medium");
+		}
+	);
 
 
-	$( "#tabs" ).tabs();
+	$("#tabs").tabs();
 
 
 	(function init() {
-	  function getTimeRemaining(endtime) {
-	    var t = Date.parse(endtime) - Date.parse(new Date());
-	    var seconds = Math.floor((t / 1000) % 60);
-	    var minutes = Math.floor((t / 1000 / 60) % 60);
-	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-	    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-	    return {
-	      'total': t,
-	      'days': days,
-	      'hours': hours,
-	      'minutes': minutes,
-	      'seconds': seconds
-	    };
-	  }
-	  
-	  function initializeClock(endtime){
-	  var timeinterval = setInterval(function(){
-	    var t = getTimeRemaining(endtime);
-	    //document.querySelector(".days > .value").innerText=t.days;
-	    document.querySelector(".hours > .value").innerText=t.hours;
-	    document.querySelector(".minutes > .value").innerText=t.minutes;
-	    document.querySelector(".seconds > .value").innerText=t.seconds;
-	    if(t.total<=0){
-	      clearInterval(timeinterval);
-	    }
-	  },1000);
-	}
-	initializeClock(((new Date()).getFullYear()+1) + "/1/1")
-	})()
- 	/*
+		function getTimeRemaining(endtime) {
+			var t = Date.parse(endtime) - Date.parse(new Date());
+			var seconds = Math.floor((t / 1000) % 60);
+			var minutes = Math.floor((t / 1000 / 60) % 60);
+			var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+			var days = Math.floor(t / (1000 * 60 * 60 * 24));
+			return {
+				'total': t,
+				'days': days,
+				'hours': hours,
+				'minutes': minutes,
+				'seconds': seconds
+			};
+		}
+
+		function initializeClock(endtime) {
+			var timeinterval = setInterval(function () {
+				var t = getTimeRemaining(endtime);
+				//document.querySelector(".days > .value").innerText=t.days;
+				document.querySelector(".hours > .value").innerText = t.hours;
+				document.querySelector(".minutes > .value").innerText = t.minutes;
+				document.querySelector(".seconds > .value").innerText = t.seconds;
+				if (t.total <= 0) {
+					clearInterval(timeinterval);
+				}
+			}, 1000);
+		}
+		initializeClock(((new Date()).getFullYear() + 1) + "/1/1");
+	})();
+	/*
 	$(document).ready(function () {
 		// Selecciona el elemento de la barra de navegación
 		var header = $("#header");
@@ -123,39 +123,38 @@
 		});
 	 });
 	 */
-	
-	 $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-300px');
-        }
-    });
-	 // Smooth scrolling on the navbar links
-	 $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 90
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$('.sticky-top').addClass('shadow-sm').css('top', '0px');
+		} else {
+			$('.sticky-top').removeClass('shadow-sm').css('top', '-300px');
+		}
+	});
+	// Smooth scrolling on the navbar links
+	$(".navbar-nav a").on('click', function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $(this.hash).offset().top - 90
+			}, 1500, 'easeInOutExpo');
+
+			if ($(this).parents('.navbar-nav').length) {
+				$('.navbar-nav .active').removeClass('active');
+				$(this).closest('a').addClass('active');
+			}
+		}
+	});
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 300) {
 			$('.back-to-top').fadeIn('slow');
 		} else {
 			$('.back-to-top').fadeOut('slow');
 		}
-		});
-		$('.back-to-top').click(function () {
-			$('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-			return false;
-		});
+	});
+	$('.back-to-top').click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+		return false;
+	});
 
 })(jQuery);
